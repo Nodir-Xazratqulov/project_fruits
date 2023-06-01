bc87ac8b611fd70c9d4ef0044b0db25641218740
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-01 23:54:49 +0500

    get-1

--- get_cheapest_fruit_id.py
+++ get_cheapest_fruit_id.py
@@ -7,5 +7,24 @@ def get_cheapest_fruit_id(data: str) -> int:
     returns:
         int: id of the cheapest fruit
     """
-    # your code here
-    pass
+    # your code 
+    f=open(data, mode='r')
+    s=f.read()
+    list1=[]
+    list2=[]
+    for i in s.split('\n'):
+        list1.append(i.split(','))
+    for k in list1:
+        list2.append(k[-1])
+    list2.pop(0)
+   
+   
+    n=(list2[0])
+    for j in list2:
+        if float(j)<float(n): 
+            n=j
+            # print(n)
+    s=list2.index(n)
+    return s
+print(get_cheapest_fruit_id('fruits.csv'))
+
