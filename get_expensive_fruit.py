005635bac73b3252c788c20253d803390faf6189
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-01 23:59:57 +0500

    get-3

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -8,6 +8,27 @@ def get_expensive_fruit(data: str) -> str:
         str: name of the most expensive fruit
     """
     # your code here
-    return  
+    f=open(data, mode='r')
+    s=f.read()
+    list1=[]
+    list2=[]
+    for i in s.split('\n'):
+        list1.append(i.split(','))
+    for k in list1:
+        list2.append(k[-1])
+    
+   
+   
+    n=(list2[1])
+    
+    for j in list2[1:]:
+        
+        if float(j)>float(n): 
+            n=j
+            
+    s=list2.index(n)
+    
+    return list1[s][0]
+print(get_expensive_fruit('fruits.csv'))
 
 
