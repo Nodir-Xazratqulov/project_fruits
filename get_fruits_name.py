5fdb85eed0420f374b7266fc5e181cf05c5558d7
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-02 00:00:32 +0500

    get-4

--- get_fruits_name.py
+++ get_fruits_name.py
@@ -7,6 +7,17 @@ def get_frutis_name(data:str)->list:
     returns:
         list: list of fruits names
     """
-    return
+    f=open(data, mode='r')
+    s=f.read()
+    list1=[]
+    list2=[]
+    for i in s.split('\n'):
+        list1.append(i.split(','))
+    for k in list1:
+        list2.append(k[0])
+    list2.pop(0)
+    list2.pop(-1)
+    return list2
+print(get_frutis_name('fruits.csv'))
 
     
