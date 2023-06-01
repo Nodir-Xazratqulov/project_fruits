5e0fbd217d2252caffaffd0ab55b1f45be8684a6
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-02 00:00:58 +0500

    get-5

--- get_total_price.py
+++ get_total_price.py
@@ -7,6 +7,20 @@ def get_total_price(data:str)->float:
     returns:
         float: fruits total price
     """
-    return
+    f=open(data, mode='r')
+    s=f.read()
+    list1=[]
+    list2=[]
+    for i in s.split('\n'):
+        list1.append(i.split(','))
+    for k in list1:
+        list2.append(k[-1])
+    list2.pop(0)
+    list2.pop(-1)
+    n=0
+    for j in list2:
+        n+=float(j)
+    return n
+print(get_total_price('fruits.csv'))
 
     
